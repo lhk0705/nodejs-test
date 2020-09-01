939a9504105929689c752ed3cd724148f4ca34bf
lhk0705 <54521195+lhk0705@users.noreply.github.com>
2020-09-02 00:11:21 +0800

    EXPRESS处理POST

--- src/expresstest.js
+++ src/expresstest.js
@@ -1,13 +1,15 @@
 var express=require('express');
 var app=express();
 var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-app.post('/addScore', urlencodedParser, function(req,res){
+var cors = require('cors');
+app.use(cors());
+app.use(bodyParser.json())
+app.post('/addScore',function(req,res){
     var response = {
-        "first_name":req.body,
-        "last_name":req.body
+        "first_name":req.body.first_name,
+        "last_name":req.body.last_name
     };
-    console.log(response);
+    console.log(response);   
     res.end(JSON.stringify(response));
 })
 app.listen(8088);
